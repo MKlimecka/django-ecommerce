e0109d6c28e601fa1798b6e91c1a4b700ea28a45
m.klimecka.ochedurska@gmail.com <m.klimecka.ochedurska@gmail.com>
2025-08-15 16:59:17 +0200

    Implemented signals for user and cart management

--- store/apps.py
+++ store/apps.py
@@ -5,5 +5,5 @@ class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
     
-    def ready(self) -> None:
+    def ready(self):
         import store.signals.hendlers
